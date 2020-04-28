53df6a36277d032738dbc8242426a19446b1af5f
vikentorpierre <vikentorpierre@gmail.com>
2020-04-27 19:07:31 -0700

    Updates

---
+++ precache-manifest.eceece9da2bde2ba459632937da7f0f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2242643d6b64af449838496170bbef14",
+    "url": "/covid19app/index.html"
+  },
+  {
+    "revision": "ccd8e5e0a022b096b5ae",
+    "url": "/covid19app/static/css/main.483169c7.chunk.css"
+  },
+  {
+    "revision": "80545bc82624b855dba6",
+    "url": "/covid19app/static/js/2.88588dd2.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19app/static/js/2.88588dd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccd8e5e0a022b096b5ae",
+    "url": "/covid19app/static/js/main.0e800c3e.chunk.js"
+  },
+  {
+    "revision": "610f029634b0adc0b8d6",
+    "url": "/covid19app/static/js/runtime-main.777e5d71.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19app/static/media/image.d7265326.png"
+  }
+]);
